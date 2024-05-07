23a86da3ca3f2f1c3999c15580173d3992ead73d
[Aniket Bhosale] <[adbhosale2707]>
2024-05-07 16:50:03 +0530

    form validation done

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -1,16 +1,39 @@
-
+"use client";
 import { createTask } from "@/utils/actions";
 import React from "react";
+import { useFormStatus,useFormState } from "react-dom";
 
-
+const initialState:any={
+  message:null,
+}
+const SubmitBtn = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      className="btn btn-primary join-item"
+      disabled={pending}
+    >
+      
+      {pending ? "crating...":"Create Task"}
+    </button>
+  );
+};
 const TodoForm = () => {
+  const [state,formAction]=useFormState(createTask,initialState)
   return (
     <form action={createTask}>
+      {state.message ? <p className="mb-2">{state.message}</p> : null}
       <div className="join w-full mb-8">
-        <input type="text" className="input input-bordered join-item w-full " placeholder="type here" name="content" required />
+        <input
+          type="text"
+          className="input input-bordered join-item w-full "
+          placeholder="type here"
+          name="content"
+          required
+        />
       </div>
-      <button type="submit" className="btn btn-primary join-item">Create Task</button>
-
+      <SubmitBtn />
     </form>
   );
 };
--- utils/actions.ts
+++ utils/actions.ts
@@ -3,19 +3,39 @@
 import prisma from "./db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-
-import { Router } from "next/router";
+import z, { string } from "zod";
 
 export const createTask = async (formData: any) => {
-  const content = formData.get("content");
-
-  await prisma.task.create({
-    data: {
-      content,
-    },
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
   });
 
-  revalidatePath("todo-list");
+  const content = formData.get("content");
+  
+  const Task = z.object({
+    content: z.string().min(5),
+  });
+
+  try {
+    Task.parse({
+      content,
+    });
+    await prisma.task.create({
+      data: {
+        content,
+      },
+    });
+
+    revalidatePath("todo-list");
+
+    return { message: "Success !" };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { message: error.message || "Error !" };
+    } else {
+      return { message: "Unknown error occurred !" };
+    }
+  }
 };
 
 export const getAllTasks = async () => {
@@ -37,31 +57,28 @@ export const deleteTask = async (formData: any) => {
   revalidatePath("/todo-list");
 };
 
-export const getTask=async(id:string)=>{
-    return await prisma.task.findUnique({
-        where:{
-            id
-        }
-    })
+export const getTask = async (id: string) => {
+  return await prisma.task.findUnique({
+    where: {
+      id,
+    },
+  });
 };
 
-export const editTask=async(formData:any)=>{
-    const id=formData.get("id")
-    const content=formData.get('content');
-    const completed=formData.get('completed');
+export const editTask = async (formData: any) => {
+  const id = formData.get("id");
+  const content = formData.get("content");
+  const completed = formData.get("completed");
 
-    await prisma.task.update({
-        where:{
-            id
-        },
-        data:{
-            content,
-            completed:completed==="on",
-        },
-        
-    });
+  await prisma.task.update({
+    where: {
+      id,
+    },
+    data: {
+      content,
+      completed: completed === "on",
+    },
+  });
 
-    redirect('/todo-list');
-
-   
+  redirect("/todo-list");
 };
