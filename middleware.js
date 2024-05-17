666e9bd90b9e6992a4153c649c38424bbb2582f9
[Aniket Bhosale] <[adbhosale2707]>
2024-05-17 22:39:18 +0530

    postgresql database published on render.com

--- middleware.js
+++ middleware.js
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server"
 
 export function middleware(request){
-    // console.log("Hello from todo-list");
+    console.log("Hello from todo-list");
 
     // return Response.json({
     //     message:'Hello there'
     // })
 
-    return NextResponse.redirect(new URL("/",request.url))
+    // return NextResponse.redirect(new URL("/",request.url))
 }
 
 export const config={
